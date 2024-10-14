035ea1e21e04e01d7315972acd8b1d51041b0742
DataJakob <lindstromjakob@hotmail.com>
2024-10-14 12:55:00 +0200

    trying out more

---
+++ first/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 2;
+            Console.WriteLine(counter);         }
+    }
+}
