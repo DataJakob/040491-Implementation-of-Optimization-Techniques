67adec4d4af24a2d2593a6df3afd8532ceea27fc
DataJakob <lindstromjakob@hotmail.com>
2024-10-17 16:54:57 +0200

    Initializing the fodler for the second prelecture work.

--- Exercises/third/Program.cs
+++ Exercises/third/Program.cs
@@ -4,7 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace third
+// Void is used as a function argument when there
+// is noe specific output, foe example a print line. 
+
+
+// Use static when there something shall be used across a whole class.
+
+// Internal for only a project and public for across multiple projects.
+
+
 {
     internal class Program
     {
